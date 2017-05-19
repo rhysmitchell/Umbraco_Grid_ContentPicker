fd0c77f410447b55a0452519868fb72d3e33f74d
Rhys.Mitchell <Rhys.Mitchell@ceredigion.gov.uk>
2017-05-19 10:15:10 +0100

    initial commit

---
+++ Editors/Controllers/GridContentPicker.Controllers.js
@@ -0,0 +1,28 @@
+﻿angular.module("umbraco").controller("GridContentPicker.Controller",
+    function ($scope, $rootScope, $routeParams, $timeout, dialogService, contentResource) {
+        $scope.setContentId = function () {
+                dialogService.contentPicker({
+                    multiPicker: true, // Specify whether to allow multiple..
+                    startNodeId: $routeParams.id, // Specify start node
+                    callback: function (data) {
+                        $scope.control.value = data;
+                        $scope.setPreview();
+                    }
+                });
+        };
+
+        $scope.setPreview = function () {
+
+            if ($scope.control.value.length > 0) {
+                $scope.control.nodes = $scope.control.value;
+            }
+        };
+
+        $timeout(function () {
+            if ($scope.control.$initializing) {
+                $scope.setContentId();
+            } else {
+                $scope.setPreview();
+            }
+        }, 200);
+    });
